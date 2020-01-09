5e412d477ed8ea9e6a505e42f8d7c99f11a0a0d0
vkumar.kumar <vkumar112@sapient.com>
2020-01-09 13:15:00 +0530

    API documentation using Swagger

--- springRestApiPractise/src/main/java/com/domain/User.java
+++ springRestApiPractise/src/main/java/com/domain/User.java
@@ -1,17 +1,23 @@
 package com.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description="All details about the user.")
 public class User {
 
     private Integer id;
 
     @Size(min=2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes="Name should have atleast 2 characters")
     private String name;
 
     @Past(message = "Birth date must be before the current date")
+    @ApiModelProperty(notes="Birth date should be in the past")
     private Date birthDate;
 
     public User() {}
